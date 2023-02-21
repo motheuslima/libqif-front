31648fd34486588b2eb8185fe8cffe1dfb1dabc4
Matheus Lima <motheuslima@gmail.com>
2023-02-21 16:34:46 +0000

    changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'libqif-api'`, () => {
+  it(`should have as title 'libqif-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('libqif-api');
+    expect(app.title).toEqual('libqif-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('libqif-api app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('libqif-front app is running!');
   });
 });
